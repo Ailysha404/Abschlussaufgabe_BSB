8cc4f8ccf932f0fbffdab74ebf3413c7c5d3c4da
Sabrina-blep <60648513+Sabrina-blep@users.noreply.github.com>
2020-02-04 11:06:49 +0100

    Add files via upload

---
+++ infos_abschlussarbeit.py
@@ -0,0 +1,17 @@
+#fastQ einlesen
+#entscheiden zwischne 33 oder 64 phred 
+#qualitätsmittelwert über alle Basen der über oderunter dem cutoff des Benutzers liegt
+#trimming von schlechten daten die schlechter sind als der angegebene Qualitätswert vom ende her getrimmt werden sollen
+#qualitätsmittelwrt in gewissen Fenster checken und die Basen dann checken, wo sie drunter liegt
+#Auswertungen: verteilung der readlängen!Readlängenbestimmung erst nach trmmenm!
+#Auswertung: GC-gehalt von jeder Sequenz 
+#Auswertungen: Qualitätsdaten, Qualität pro basenposition, Qualität por Sequenz
+#Graphen und plots als graphische representation!!
+#Optionsmanagment:kein Input!!!!, main()keine globalen variablen,interaktiv nur als Bonus!,argparse oder docopt
+#dupplicatet sequenzes
+#kmerstatistik welche sind überrepresentiert, welche weniger representiert
+#foo.py -f 30 -t 20 -p 33
+#foo.py -i
+#Termin: 21.02.19 Abgabe: bis zum Morgen am präsentationstag aber muss per email abgeschickt werden!
+#live präsentation ca. 20 minuten
+#freitags arbeiten im Kursraum möglich, wenn vorher angeklärt
