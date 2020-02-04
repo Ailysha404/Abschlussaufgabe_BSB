eed5a2cdfbdd4c0b10f5175a0ce3bdc810eb8b67
Ailysha404 <60096513+Ailysha404@users.noreply.github.com>
2020-02-04 10:54:00 +0100

    Add files via upload

---
+++ my_abschlussaufgabe.py
@@ -0,0 +1,23 @@
+#unterscheidung phred 64 or 32
+#dictionary mit alphabet zu qualität
+#aber nich gesamte sequenz überprüfen, ab gewissem unsicherheitspunkt fragen
+#qualitätskontrolle, cut-off vom user angegeben, sonst rausgeworfen
+#bei trimming bestimmen härte des cut-offs
+#evtl erst ab nachfolgenden schlechten qualitäten, oder mittelwert fenster
+#auswertung für: verteilung der readlängen mit mw, median, stabw
+#gc-gehalt
+#qualitätsdaten mit mw, pro base, pro sequenz, gesamtfenster pro base
+#graphische darstellung, evtl plotly?
+#evtl prüfen auf Ns, evtl k-mere?
+#optionsmanagement über kommandozeilen-parameter - interaktiver und nicht interaktiver modus - bei standard KEINE nutzerabfragen
+#evtl datei mit output speichern zur nachverfolgung
+#präsentation 21.02, ~20 min als team, abgabe am tag selbst 
+
+import argparse
+import matplotlib.pyplot as plt
+
+def __main__():
+    pass
+
+if __name__ == "__main__":
+    main()
