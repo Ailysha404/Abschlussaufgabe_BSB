9e1027102403f08a53808c9bad4235a431927a7a
Ailysha404 <60096513+Ailysha404@users.noreply.github.com>
2020-02-09 11:24:12 +0100

    Update my_abschlussaufgabe.py

--- my_abschlussaufgabe.py
+++ my_abschlussaufgabe.py
@@ -1,83 +1,95 @@
-#unterscheidung phred 64 or 32
-#dictionary mit alphabet zu qualität
-#aber nich gesamte sequenz überprüfen, ab gewissem unsicherheitspunkt fragen
-#qualitätskontrolle, cut-off vom user angegeben, sonst rausgeworfen
-#bei trimming bestimmen härte des cut-offs
-#evtl erst ab nachfolgenden schlechten qualitäten, oder mittelwert fenster
-#auswertung für: verteilung der readlängen mit mw, median, stabw
-#gc-gehalt
-#qualitätsdaten mit mw, pro base, pro sequenz, gesamtfenster pro base
-#graphische darstellung, evtl plotly?
-#evtl prüfen auf Ns, evtl k-mere?
-#optionsmanagement über kommandozeilen-parameter - interaktiver und nicht interaktiver modus - bei standard KEINE nutzerabfragen
-#evtl datei mit output speichern zur nachverfolgung
-#präsentation 21.02, ~20 min als team, abgabe am tag selbst
-
-#idea: skip upper comment lines by for loop and if for @
-#readlines writing with if condition? eg only saving 3 of the 4 lines?
-
 import sys
 import argparse
 import matplotlib.pyplot as plt
 
-def Phred_Bestimmung(datei):
-    alphabet = {} #Alphabet ascii-zeichen, score
-    score_raw = "" #read line, string ascii
-    pass
+class Read: #class for all reads
+	def __init__(self,content,id,phred,ascii,idlist):
+		self.length = len(content[1])
+		self.gc = (content[1].count("G")+content[1].count("C"))/self.length #in decimal - between 0 and 1
+		self.name = content[0].split()[0] #how to get specific substring from line 1? #saves own id as name
+		self.sequenz = content[1] #sequence-string
+		self.quality = self.qualitaet(content,phred,ascii) #list of quality-scores
+
+	def qualitaet(self,content,ascii,phred):
+		values = {}
+		values[content[0]] = content[3] #keys ids, values score
+
+		if phred == "33": #check for phred format
+			alphabet_33 = {char:value-33 for value,char in enumerate(ascii,33) if value < 74} #make dictionary w ascii-character + phred score
+
+			for score_string in alphabet_33: #id read
+				score_list = [] #list with all scores from the read
+
+				for letter in alphabet_33[score_string].rstrip(): #individual letter in score line
+					score_list.append(alphabet_33[str(letter)]) #add score to list
+					values[score_string.rstrip()] = score_list  #dictionary with id string as key, list with scores as value
+
+		elif phred == "64":
+			alphabet_64 = {char:value-64 for value,char in enumerate(ascii,33) if value > 63 and value < 105} #make dictionary w ascii-character + phred score
+
+			for score_string in alphabet_64:
+				score_list = []
+
+				for letter in content[3]:
+					score_list.append(alphabet_64[str(letter)])
+					values[content[0]] = score_list
+
+		else:
+			quit()
+
+		return values
+
+def Phred_Bestimmung(datei,ascii,phred=None):
+
+	if phred == None: #if not manually given phred, check first sequence for clarification
+		for score_char in datei:
+
+			if score_char in [char for value,char in enumerate(ascii,33) if value < 64]:
+				phred = "33"
+				break
+
+			elif score_char in [char for value,char in enumerate(ascii,33) if value > 63 and value < 105]:
+				phred = "64"
+				break
+
+			else:
+				quit()
+
+	return phred
 
 def Parser():
-    input_parser = argparse.ArgumentParser(description="Qualitätsauswertung für FASTA-Dateien")
-    input_parser.add_argument("Dateipfad", help="Dateipfad zur FASTA-Datei")
-    input_parser.add_argument('-p','--phred', dest='opt', help="Optionale manuelle Angabe des Phred-Formats, 33 oder 64")
-    input_parser.add_argument('-i','--interaktiv', dest='interaktiv', action="store_true", help="Aktivierung des kommentierten interaktiven Modus")
-    arguments = input_parser.parse_args() #Erstellen dictionary, Zugriff auf Argument über Namen
-    return arguments
-    pass
+	input_parser = argparse.ArgumentParser(description="Qualitätsauswertung für FASTA-Dateien")
+	input_parser.add_argument("Dateipfad", help="Dateipfad zur FASTA-Datei")
+	input_parser.add_argument('-p','--phred', dest='opt', help="Optionale manuelle Angabe des Phred-Formats, 33 oder 64")
+	input_parser.add_argument('-t','--trim', dest='trim_val', help="Optionale manuelle Angabe des Trimming-Scores",default=25)
+	input_parser.add_argument('-i','--interaktiv', dest='interaktiv', action="store_true", help="Aktivierung des kommentierten interaktiven Modus")
+	arguments = input_parser.parse_args() #Erstellen dictionary, Zugriff auf Argument über Namen
 
-def Qualitaet(datei,phred=None):
-    values = {}
-    ascii = ("!\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~")
-    alphabet_33 = {char:value-33 for value,char in enumerate(ascii,33) if value < 74} #make dictionary w ascii-character + phred score
-    alphabet_64 = {char:value-64 for value,char in enumerate(ascii,33) if value > 63 and value < 105} #make dictionary w ascii-character + phred score
-    scores_ascii = dict(zip(datei[0::4],datei[3::4])) #keys ids, values score
-    if phred == None: #if not manually given phred, check first sequence for clarification
-        for score_char in scores_ascii.values():
-            if score_char in alphabet_33.keys():
-                phred = "33"
-
-            elif score_char in alphabet_64.keys():
-                phred = "64"
-
-            else:
-                quit()
-
-    if phred == "33": #check for phred format
-        for score_string in scores_ascii: #id sequence
-            for letter in scores_ascii[score_string]: #individual letter in score line
-                print(f"{letter}, {alphabet_33[str(letter)]}")
-
-    elif phred == "64":
-        for score_string in scores_ascii:
-            score_list = []
-            for letter in scores_ascii[score_string].rstrip(): #return score for each ascii
-                score_list.append(alphabet_64[str(letter)]) #
-                values[score_string+letter] = score_list
-
-    return values
+	return arguments
 
 def __main__():
+	all_ids = [] #! saved as object code, not as string! id by item.name
+	ascii = ("!\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~")
+	arguments = Parser()
+	phred = arguments.opt
 
-    arguments = Parser()
-    with open(arguments.Dateipfad) as inhalt:
-        for lines in inhalt:
-            
-    #file = open(arguments.Dateipfad)
-    #inhalt = file.readlines()[0:8] #lines als Liste gespeichert - 0:id 1:seq 2:id 3:score und multiple...
-    #file.close()
-    print(Qualitaet(inhalt,arguments.opt))
-    #scores_seq = Qualitaet(inhalt) #Dictionary mit key ids, values score
-    #print(f"{scores_seq.keys()} : {scores_seq.values()}")
-    pass
+	with open(arguments.Dateipfad) as inhalt: #open user-provided file path
+		line_pack = []
+		test_cnt = 0	#!! NUR KÜRZEN FÜR TESTLÄUFE
+		for lines in inhalt:
+			if line_pack == []:
+				phred = Phred_Bestimmung(lines,ascii,phred)
+			line_pack.append(lines.rstrip())
+			test_cnt += 1
+			if len(line_pack) == 4:
+				id = lines[0]
+				all_ids.append(Read(line_pack,id,ascii,phred,all_ids))
+				line_pack = []
+			if test_cnt > 140:	#!! NUR KÜRZEN FÜR TESTLÄUFE
+				break
+
+	for item in all_ids:
+		print(phred)
 
 if __name__ == "__main__":
-    __main__()
+	__main__()
