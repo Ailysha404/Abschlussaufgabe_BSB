028a50d0c1c4dc9e0bcf4238b68efca26f064b64
Ailysha404 <60096513+Ailysha404@users.noreply.github.com>
2020-02-10 14:08:13 +0100

    Update my_abschlussaufgabe.py

--- my_abschlussaufgabe.py
+++ my_abschlussaufgabe.py
@@ -7,12 +7,12 @@ class Read: #class for all reads
 		self.length = len(content[1])
 		self.gc = (content[1].count("G")+content[1].count("C"))/self.length #in decimal - between 0 and 1
 		self.name = content[0].split()[0] #how to get specific substring from line 1? #saves own id as name
-		self.sequenz = content[1] #sequence-string
-		self.quality = self.qualitaet(content,phred,ascii) #list of quality-scores
+
+		self.quality = trimming(self.qualitaet(content,phred,ascii)[self]) #dict of name with quality-scores
+		self.sequenz = content[1][0:len(self.quality)] #sequence-string
 
 	def qualitaet(self,content,ascii,phred):
 		values = {}
-		values[content[0]] = content[3] #keys ids, values score
 
 		if phred == "33": #check for phred format
 			alphabet_33 = {char:value-33 for value,char in enumerate(ascii,33) if value < 74} #make dictionary w ascii-character + phred score
@@ -22,7 +22,7 @@ class Read: #class for all reads
 
 				for letter in alphabet_33[score_string].rstrip(): #individual letter in score line
 					score_list.append(alphabet_33[str(letter)]) #add score to list
-					values[score_string.rstrip()] = score_list  #dictionary with id string as key, list with scores as value
+					values[self] = score_list  #dictionary with id string as key, list with scores as value
 
 		elif phred == "64":
 			alphabet_64 = {char:value-64 for value,char in enumerate(ascii,33) if value > 63 and value < 105} #make dictionary w ascii-character + phred score
@@ -32,7 +32,7 @@ class Read: #class for all reads
 
 				for letter in content[3]:
 					score_list.append(alphabet_64[str(letter)])
-					values[content[0]] = score_list
+					values[self] = score_list
 
 		else:
 			quit()
@@ -60,36 +60,91 @@ def Phred_Bestimmung(datei,ascii,phred=None):
 def Parser():
 	input_parser = argparse.ArgumentParser(description="Qualitätsauswertung für FASTA-Dateien")
 	input_parser.add_argument("Dateipfad", help="Dateipfad zur FASTA-Datei")
-	input_parser.add_argument('-p','--phred', dest='opt', help="Optionale manuelle Angabe des Phred-Formats, 33 oder 64")
+	input_parser.add_argument('-p','--phred', dest='phred', help="Optionale manuelle Angabe des Phred-Formats, 33 oder 64")
 	input_parser.add_argument('-t','--trim', dest='trim_val', help="Optionale manuelle Angabe des Trimming-Scores",default=25)
+	input_parser.add_argument('-s','--save', dest='save', help="Optionale Angabe eines Dateipfades zum Speichern der Daten")
 	input_parser.add_argument('-i','--interaktiv', dest='interaktiv', action="store_true", help="Aktivierung des kommentierten interaktiven Modus")
 	arguments = input_parser.parse_args() #Erstellen dictionary, Zugriff auf Argument über Namen
 
 	return arguments
 
+def trimming(scores):
+	counter = 0
+	summe = 0
+	item_index = 0
+	kmer_neu = []
+
+	for kmer in scores:
+		counter += 1
+		summe += kmer
+		item_index += 1
+
+		if counter == 3:
+			kmer_mean=summe/3
+			counter=0
+			summe=0 #Summe wieder zurücksetzen!
+
+			if kmer_mean < 25:
+				kmer_neu = scores[0:item_index]
+				return kmer_neu
+	else:
+		return scores
+
+def graph_gc(dict_reads,fig):
+	fig.add_subplot(4,2,(1,4))
+	X=[]
+	for value in dict_reads:
+		X.append(value.gc*100)
+
+	plt.hist(X,histtype="step",color="blue")
+	plt.xlabel('GC Content (%)')
+	plt.ylabel('Count')
+	plt.title('GC Distribution over all Sequences')
+	return plt
+
+def graph_scores(dict_reads,fig):
+	fig.add_subplot(4,2,(5,8))
+	X=[]
+	for item in dict_reads:
+		overall_score = 0
+		for score in item.quality:
+			overall_score += score
+		X.append(overall_score/len(item.quality))
+
+	plt.hist(X,histtype="step",color="orange",label="Average Quality per Read",lw=2)
+	plt.xlabel('Mean Sequence Quality')
+	plt.ylabel('Reads')
+	plt.title('Quality Score Distribution over all Sequences')
+	return plt
+
 def __main__():
-	all_ids = [] #! saved as object code, not as string! id by item.name
+	all_ids = [] #! saved as object code, not as string! saved in item.name
 	ascii = ("!\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~")
 	arguments = Parser()
-	phred = arguments.opt
+	phred = arguments.phred
+	count = 0
 
 	with open(arguments.Dateipfad) as inhalt: #open user-provided file path
 		line_pack = []
-		test_cnt = 0	#!! NUR KÜRZEN FÜR TESTLÄUFE
+		test_cnt = 0	#!! KÜRZEN FÜR TESTLÄUFE
 		for lines in inhalt:
 			if line_pack == []:
 				phred = Phred_Bestimmung(lines,ascii,phred)
 			line_pack.append(lines.rstrip())
-			test_cnt += 1
+			test_cnt += 1	#!! KÜRZEN FÜR TESTLÄUFE
 			if len(line_pack) == 4:
 				id = lines[0]
 				all_ids.append(Read(line_pack,id,ascii,phred,all_ids))
 				line_pack = []
-			if test_cnt > 140:	#!! NUR KÜRZEN FÜR TESTLÄUFE
+			if test_cnt > 10000:	#!! KÜRZEN FÜR TESTLÄUFE
 				break
 
-	for item in all_ids:
-		print(phred)
+	fig = plt.figure()	#create canvas for graphs
+	graph_gc(all_ids,fig)	#create first subplot, for gc-percentages
+	graph_scores(all_ids,fig)	#fill in second subplot, for scores
+	plt.show()
+	# if arguments.save:
+	# 	plt.savefig(str(arguments.save))
 
 if __name__ == "__main__":
 	__main__()
